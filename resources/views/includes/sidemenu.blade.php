920da3458eb2deeeb8fab2546cc9e8426e8d7ad7
abcdefghijkok <aaa111sssbbbb@gmail.com>
2024-12-14 01:42:02 +0900

    update:レイアウト一新

--- resources/views/includes/sidemenu.blade.php
+++ resources/views/includes/sidemenu.blade.php
@@ -3,21 +3,36 @@
     <!-- ページに飛んだあとクリックイベントで移動する -->
     <a class="nav-link" href="/#section3">【プロフィール】</a>
     <a class="nav-link" href="/#section5">【実習履歴】</a>
-    <a class="nav-link" href="/#section2">【Java課題制作】</a>
-    <a class="nav-link" href="/#section1">【PHP課題制作】</a>
+
+    <!-- <a class="nav-link" href="/#section2">【Java課題制作】</a> -->
+    <a class="nav-link" href="#section2" @click="toggleSubMenu('java')">【Java課題制作】
+        <span class="float-end" v-if="!menu.java"><i class="fas fa-chevron-down"></i></span>
+        <span class="float-end" v-else><i class="fas fa-chevron-right"></i></span>
+    </a>
+    <div v-if="menu.java">
+        <a class="nav-link ms-3 ms-1" href="/#section12">FizzBuzz</a>
+        <a class="nav-link ms-3 ms-1" href="/#section13">青森鹿児島問題</a>
+        <a class="nav-link ms-3 ms-1" href="/#section14">ブラックジャック</a>
+    </div>
+
+    <!-- <a class="nav-link" href="/#section1">【PHP課題制作】</a> -->
+    <a class="nav-link" href="/#section1" @click="toggleSubMenu('php')">【PHP課題制作】
+        <span class="float-end" v-if="!menu.php"><i class="fas fa-chevron-down"></i></span>
+        <span class="float-end" v-else><i class="fas fa-chevron-right"></i></span>
+    </a>
+    <div v-if="menu.php">
+        <a class="nav-link ms-3 ms-1" href="/#section6">メルカリ風フリマアプリ</a>
+        <a class="nav-link ms-3 ms-1" href="/#section7">SNS風アプリ</a>
+        <a class="nav-link ms-3 ms-1" href="/#section8">本管理アプリ</a>
+        <a class="nav-link ms-3 ms-1" href="/#section9">顧客管理システム改修</a>
+        <a class="nav-link ms-3 ms-1" href="/#section10">簡易お問い合わせフォーム</a>
+        <a class="nav-link ms-3 ms-1" href="/#section11">初めてデプロイに挑戦</a>
+    </div>
+
     <a class="nav-link" href="/#section4">【プログラミング学習履歴】</a>
 
-    <a class="nav-link" href="#" @click="toggleSubMenu('location')">学習したプログラミング
-        <span class="float-end" v-if="menu.employeeMaster">-</span>
-        <span class="float-end" v-else>+</span>
-    </a>
-    <div v-if="menu.location">
-        <a class="nav-link ms-3 ms-5" href="{{ route('languages.index') }}">プログラミング言語</a>
-        <a class="nav-link ms-3 ms-5" href="{{ route('materials.index') }}">使用した教材</a>
-        <a class="nav-link ms-3 ms-5" href="{{ route('learning-days.index') }}">学習した日数</a>
-        <a class="nav-link ms-3 ms-5" href="{{ route('artifacts.index') }}">作成した成果物</a>
-    </div>
-    <div style="background-color: #ffffff;">
+
+    <div style="background-color: rgb(253, 253, 253)">
         <div style="padding-top: 0.5rem;">
             <strong class="sepia-bg2"><span>全アプリ共通のアカウント</span></strong>
         </div>
@@ -33,5 +48,16 @@
             <span style="color: #007BFF; text-decoration: underline;">青字</span>
             をクリックすると詳細へ
         </p>
+    </div>
+    <!-- 学習したプログラミング -->
+    <a class="nav-link" href="#" @click="toggleSubMenu('location')">顧客管理システム改修
+        <span class="float-end" v-if="!menu.location"><i class="fas fa-chevron-down"></i></span>
+        <span class="float-end" v-else><i class="fas fa-chevron-right"></i></span>
+    </a>
+    <div v-if="menu.location">
+        <a class="nav-link ms-3 ms-1" href="{{ route('languages.index') }}">プログラミング言語（リンク）</a>
+        <a class="nav-link ms-3 ms-1" href="{{ route('materials.index') }}">使用した教材（リンク）</a>
+        <a class="nav-link ms-3 ms-1" href="{{ route('learning-days.index') }}">学習した日数（リンク）</a>
+        <a class="nav-link ms-3 ms-1" href="{{ route('artifacts.index') }}">作成した成果物（リンク）</a>
     </div>
 </nav>
